8125454c943c9dd926cc899739bba7b60493b1ca
adityadesh09 <aditya@s2q2.com>
2022-12-06 11:39:28 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:s2q2_website/LandingPage/LandingPage.dart';
+import 'package:s2q2_website/Navbar/Navbar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 's2q2 website',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Montserrat"),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromARGB(255, 17, 154, 234),
+                Color.fromARGB(255, 8, 89, 137),
+                Color.fromRGBO(36, 11, 54, 1.0)
+              ]),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Navbar(),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 40.0),
+                child: LandingPage(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
